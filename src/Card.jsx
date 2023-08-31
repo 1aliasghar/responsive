4b412d034ecacc5916ad44ec14544d6b26784627
Ali <93658046+1aliasghar@users.noreply.github.com>
2023-08-31 17:40:05 +0500

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -11,8 +11,8 @@ const Card = (props) => {
                 <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc}/>
                 <div className="card-body">
                     <h5 className="card-title font-weight-bold">{props.title}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <NavLink to="/about" className="btn btn-primary">Go somewhere</NavLink>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                    <NavLink to="/about" className="btn btn-primary">Go there</NavLink>
                 </div>
             </div>
         </div>
